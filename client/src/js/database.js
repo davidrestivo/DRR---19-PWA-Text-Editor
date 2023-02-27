d310454caa4c70ceed4d988e4dacd1d708bde131
David Restivo <davidrestivo27@gmail.com>
2023-02-26 23:51:31 -0500

    added code for program

---
+++ client/src/js/database.js
@@ -0,0 +1,43 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+// export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction('jate', 'readwrite');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('Data saved to database', result.value);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+// export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  const txtEditorDB = await openDB('jate', 1);
+  const txt = txtEditorDB.transaction('jate', 'readonly');
+  const storeVar = txt.objectStore('jate');
+  const request = storeVar.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
+    return result?.value;
+};
+
+initdb();
